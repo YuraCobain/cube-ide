a13349d3b9bb307330b1f53be20811fe2c693fa8
Yura Cobain <yura.cobain@gmail.com>
2022-10-06 13:18:17 +0000

    add first working version

---
+++ install_cude_ide.py
@@ -0,0 +1,113 @@
+import os
+import subprocess
+import os
+
+HOME_DIR = os.path.expanduser('~')
+
+class AwesomeVimrc():
+    git_clone_cmd = 'git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime'
+    install_cmd = "sh ~/.vim_runtime/install_awesome_vimrc.sh"
+
+    def is_installed(self):
+        return os.path.exists(f'{HOME_DIR}/.vim_runtime')
+
+    def clone(self):
+        subprocess.check_call(self.git_clone_cmd, shell=True)
+
+    def install(self):
+        subprocess.check_call(self.install_cmd, shell=True)
+
+    def post_configure(self):
+        subprocess.check_call("cp my_configs.vim ~/.vim_runtime/", shell=True)
+
+class VimYCM:
+    git_clone_cmd = 'git clone https://github.com/ycm-core/YouCompleteMe.git'
+    git_submodule_cmd ='git submodule update --init --recursive'
+    install_cmd = "python3 install.py --clangd-completer  "
+
+    def is_installed(self):
+        return os.path.exists(f'{HOME_DIR}/.vim_runtime/my_plugins/YouCompleteMe')
+
+    def clone(self):
+        os.chdir(f'{HOME_DIR}/.vim_runtime/my_plugins/')
+        subprocess.check_call(self.git_clone_cmd, shell=True)
+        os.chdir(f'{HOME_DIR}/.vim_runtime/my_plugins/YouCompleteMe')
+        subprocess.check_call(self.git_submodule_cmd, shell=True)
+
+    def install(self):
+        install_cmd = self.install_cmd
+        if HOME_DIR == "/root":
+            install_cmd += " --force-sudo"
+
+        subprocess.check_call(install_cmd, shell=True)
+
+    def post_configure(self):
+        pass
+
+class VimClangFormat:
+    git_clone_cmd = 'git clone https://github.com/rhysd/vim-clang-format.git'
+    install_cmd = "sudo apt install clang-format"
+
+    def is_installed(self):
+        return os.path.exists(f'{HOME_DIR}/.vim_runtime/my_plugins/vim-clang-format')
+
+    def clone(self):
+        os.chdir(f'{HOME_DIR}/.vim_runtime/my_plugins/')
+        subprocess.check_call(self.git_clone_cmd, shell=True)
+        os.chdir(f'{HOME_DIR}/.vim_runtime/my_plugins/vim-clang-format')
+
+    def install(self):
+        subprocess.check_call(self.install_cmd, shell=True)
+
+    def post_configure(self):
+        pass
+
+class VimInspector:
+    git_clone_cmd = 'git clone https://github.com/puremourning/vimspector.git'
+
+    def is_installed(self):
+        return os.path.exists(f'{HOME_DIR}/.vim_runtime/my_plugins/vimspector')
+
+    def clone(self):
+        os.chdir(f'{HOME_DIR}/.vim_runtime/my_plugins/')
+        subprocess.check_call(self.git_clone_cmd, shell=True)
+
+    def install(self):
+        pass
+
+    def post_configure(self):
+        pass
+
+class Fzf:
+    git_clone_cmd = 'git clone https://github.com/junegunn/fzf.git ~/.fzf'
+    install_cmd = "yes | ~/.fzf/install"
+
+    def is_installed(self):
+        return os.path.exists(f'{HOME_DIR}/.fzf')
+
+    def clone(self):
+        subprocess.check_call(self.git_clone_cmd, shell=True)
+
+    def install(self):
+        subprocess.check_call(self.install_cmd, shell=True)
+
+    def post_configure(self):
+        pass
+
+packages = [AwesomeVimrc(), VimYCM(), VimClangFormat(), VimClangFormat(), Fzf()]
+
+def setup_package(package):
+    print(package.__class__.__name__)
+    if package.is_installed():
+        print("already is installed. pass.")
+        return
+
+    package.clone()
+    package.install()
+    package.post_configure()
+
+def main():
+    [setup_package(package) for package in packages]
+
+if __name__ == '__main__':
+    main()
--- my_configs.vim
+++ my_configs.vim
@@ -1,4 +1,5 @@
 set number
+set gfn=IBM\ Plex\ Mono\ 10
 
 " YCM mapping
 nnoremap <leader>yl :YcmCompleter GoToDeclaration<CR>
@@ -6,6 +7,24 @@ nnoremap <leader>yf :YcmCompleter GoToDefinition<CR>
 nnoremap <leader>yy :YcmCompleter GoToDefinitionElseDeclaration<CR>
 nnoremap <leader>ys :YcmCompleter GoToReferences<CR>
 nnoremap <leader>yr :<c-u>YcmCompleter RefactorRename <C-R>=Abolish.Coercions.s(expand("<cword>"))<CR>
+
+" Close preview window after completing the insertion
+let g:ycm_autoclose_preview_window_after_insertion = 1
+let g:ycm_autoclose_preview_window_after_completion = 1
+
+let g:ycm_confirm_extra_conf = 0                " Don't confirm python conf
+let g:ycm_always_populate_location_list = 1     " Always populae diagnostics list
+let g:ycm_enable_diagnostic_signs = 1           " Enable line highligting diagnostics
+let g:ycm_open_loclist_on_ycm_diags = 1         " Open location list to view diagnostics
+
+let g:ycm_max_num_candidates = 20               " Max number of completion suggestions 
+let g:ycm_max_num_identifier_candidates = 10    " Max number of identifier-based suggestions
+let g:ycm_auto_trigger = 1                      " Enable completion menu
+let g:ycm_show_diagnostic_ui = 1                " Show diagnostic display features
+let g:ycm_error_symbol = '>>'                   " The error symbol in Vim gutter
+let g:ycm_enable_diagnostic_signs = 1           " Display icons in Vim's gutter, error, warnings
+let g:ycm_enable_diagnostic_highlighting = 1    " Highlight regions of diagnostic text
+let g:ycm_echo_current_diagnostic = 1           " Echo line's diagnostic that cursor is on
 
 set completeopt-=preview
 
@@ -29,3 +48,7 @@ autocmd FileType c,cpp,objc map <buffer><Leader>x <Plug>(operator-clang-format)
 " Toggle auto formatting:
 nmap <Leader>C :ClangFormatAutoToggle<CR>
 autocmd FileType c ClangFormatAutoEnable
+
+autocmd VimEnter * NERDTree
+
+
